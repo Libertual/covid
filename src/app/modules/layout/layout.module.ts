41242f5252f661bbd4844a18613b1f1d886a5132
Tecnual <pedro@tecnual.com>
2020-04-09 13:18:13 +0200

    feat: New footer. Description, contact and sources.

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,11 +10,13 @@ import { HeaderComponent } from './header/header.component';
   declarations: [FooterComponent, HeaderComponent],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   exports: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    MatIconModule
    ]
 })
 export class LayoutModule { }
